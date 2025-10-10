ed1a9812401c4cdf0b7e3dbc40b57785aeb924df
arnmap <arnmap@proton.me>
2025-10-10 13:35:20 -0500

    Additional prototype refactoring
    Tested locally without AWS credentials.

--- arnmap.py
+++ arnmap.py
@@ -2,33 +2,42 @@
 
 import arnmap_helper
 
-class arnmap:
+class ArnMap:
+
+	def __init__(self):
+		
+		self.arn_structure_dict = {
+			'prefix': 0,
+			'partition': 1,
+			'service': 2,
+			'region': 3,
+			'accountid': 4,
+			'resource': 5
+		}
 
 	def scan(self, arn):
 		"""Get data from boto3 about the given ARN"""
-		
-		index_resource = 0
-		index_internal_state = 1		
-		index_resource_type = 2
 
-		arn_list = self._arnmap__verify_arn(arn)
+		arn_components_list = self.__verify_arn(arn)
 		
 		method_name = (
 			"scan_" 
-			+ str(arn_list[index_resource_type])
+			+ str(arn_components_list[self.arn_structure_dict.get("service")])
 		)
 		
-		scan_output = ""
+		scan_data_str = ""
+		scan_output_str = ""
 				
 		try:
 	
 			if hasattr(arnmap_helper, method_name) and callable(getattr(arnmap_helper, method_name)):
 				
-				resource_scan = getattr(arnmap_helper, method_name)(arn, arn_list)
-				resource_dict = resource_scan[index_resource]
-				resource_internal_state = resource_scan[index_internal_state]
+				resource_scan = getattr(arnmap_helper, method_name)(arn, arn_components_list, self.arn_structure_dict)
 				
-				if not resource_dict:
+				scan_data_list = resource_scan[0]
+				resource_internal_state = resource_scan[1]
+				
+				if not scan_data_list:
 					
 					resource_status = "NOT_FOUND"
 					
@@ -43,75 +52,75 @@ class arnmap:
 						+ resource_internal_state 
 						+ "]"
 					)
-								
-				scan_output = (
+
+				for scan_data_dict in scan_data_list:
+					scan_data_str += str(scan_data_dict)
+					
+				scan_output_str = (
 					"arn: " 
 					+ arn 
 					+ "\n" 
 					+ "resource_status: " 
 					+ resource_status
 					+ "\n"
-					+ str(resource_dict)
+					+ scan_data_str
 				)
 				
 			else:
 				
-				scan_output = (
+				scan_output_str = (
 					"Method does not exist in arnmap_module.py or is not callable: " 
 					+ method_name
 				)				
 				
-			return scan_output
+			return scan_output_str
 			
 		except Exception as e:
 			
-			scan_output = (
+			scan_output_str = (
 				"Exception: " 
 				+ type(e).__name__ 
 				+ " - " 
 				+ str(e)
 			)
 			
-			return scan_output
+			return scan_output_str
 
 
 	def __verify_arn(self, arn):
 		"""Confirm that arn is correct format and return a parsed list of elements"""
-		
-		index_arn = 0
-		index_aws = 1
 								
 		if arn.count(":") < 5:
 			return []				
 		
-		arn_list = arn.split(":")
+		components_list = arn.split(":")
 		
-		if arn_list[index_arn] != "arn" or arn_list[index_aws] != "aws":
+		if components_list[self.arn_structure_dict.get("prefix")] != "arn" or components_list[self.arn_structure_dict.get("partition")] != "aws":
 			return []
 			
-		return arn_list
+		return components_list
 		
 		
-	def __main(args):
-		"""Standard main method within the class. Only called when the program is run 
-		directly. Allows execution of code related to the class. While still being able 
-		to import the class in other modules without execution of the main method."""
-		
-		import sys
-		if not args.arn:
-			sys.exit(1)		
-		print("args.arn:", args.arn)
-		# Loop through array of arn strings	
-			# arnmap = arnmap()
-			# output = arnmap.scan("arn:aws:glue:us-east-1:123456789012:job/test")
-			# print(output)    
-		
+def __main(args):
+	"""Standard main method within the class. Only called when the program is run 
+	directly. Allows execution of code related to the class. While still being able 
+	to import the class in other modules without execution of the main method."""
+	
+	import sys
+	if not args.arn:
+		sys.exit(1)		
+	# print("args.arn:", args.arn)
+	
+	arnmap = ArnMap()
+	output = arnmap.scan("arn:aws:glue:us-east-1:123456789012:job/test")
+	print(output)  				
 
-	if __name__ == "__main__":
-		"""This is the entry point when the program is run directly."""
-		
-		import argparse
-		parser = argparse.ArgumentParser(description='Scan AWS resources by ARN.')
-		parser.add_argument('--arn', nargs='*', help='list of Amazon Resource Names: "arn1" "arn2" ...', default=[], required=True)
-		args = parser.parse_args()
-		__main(args)		
+
+if __name__ == "__main__":
+	"""This is the entry point when the program is run directly."""
+	
+	import argparse
+	parser = argparse.ArgumentParser(description='Scan AWS resources by ARN.')
+	parser.add_argument('--arn', nargs='*', help='list of Amazon Resource Names: "arn1" "arn2" ...', default=[], required=True)
+	args = parser.parse_args()
+	__main(args)		
