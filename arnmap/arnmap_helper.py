7bbf86af4306ccb796fb301547df343400b72d03
arnmap <arnmap@proton.me>
2026-09-04 16:44:23 -0500

    Add decorator for client setup and error handling

--- arnmap/arnmap_helper.py
+++ arnmap/arnmap_helper.py
@@ -1,18 +1,46 @@
 # arnmap_helper.py
 
+from functools import wraps
 import boto3
+from botocore.exceptions import ClientError
 
 
-def scan_dms(arn, arn_components_list, arn_structure_dict):
+def scanner_aws(service_name):
+	"""Decorator to handle generic client setup and error handling."""
+
+	def decorator_scanner(scanner_func):
+
+		@wraps(scanner_func)
+		def wrapper_scanner(arn, arn_components_list, arn_structure_dict):
+
+			resource_type, resource_name = get_resource_structure(arn, arn_components_list, arn_structure_dict)
+			region = arn_components_list[arn_structure_dict.get("region")]
+
+			try:
+
+				session = boto3.Session(region_name=region)
+				client = session.client(service_name)
+				return scanner_func(client, resource_type, resource_name, arn)
+
+			except ClientError as e:
+
+				error_code = e.response.get('Error', {}).get('Code', '')
+				if 'NotFound' in error_code or 'NoSuch' in error_code:
+					return [], ""
+				raise e
+
+		return wrapper_scanner
+
+	return decorator_scanner
+
+
+@scanner_aws('dms')
+def scan_dms(client, resource_type, resource_name, arn):
 	"""Get data from boto3 (dms) about the given ARN"""
 
 	scan_data_list = []
 	resource_dict = {}
 	resource_internal_state = ""
-
-	resource_type, resource_name = get_resource_structure(arn, arn_components_list, arn_structure_dict)
-	session = boto3.Session(region_name=arn_components_list[arn_structure_dict.get("region")])
-	client = session.client('dms')
 
 	if resource_type == "task":
 
@@ -42,16 +70,13 @@ def scan_dms(arn, arn_components_list, arn_structure_dict):
 	return [scan_data_list, resource_internal_state]
 
 
-def scan_ec2(arn, arn_components_list, arn_structure_dict):
+@scanner_aws('ec2')
+def scan_ec2(client, resource_type, resource_name, arn):
 	"""Get data from boto3 (ec2) about the given ARN"""
 
 	scan_data_list = []
 	resource_dict = {}
 	resource_internal_state = ""
-
-	resource_type, resource_name = get_resource_structure(arn, arn_components_list, arn_structure_dict)
-	session = boto3.Session(region_name=arn_components_list[arn_structure_dict.get("region")])
-	client = session.client('ec2')
 
 	if resource_type == "instance":
 
@@ -75,60 +100,55 @@ def scan_ec2(arn, arn_components_list, arn_structure_dict):
 	# list[list[dict], string]
 	return [scan_data_list, resource_internal_state]
 
-def scan_glue(arn, arn_components_list, arn_structure_dict):
+
+@scanner_aws('glue')
+def scan_glue(client, resource_type, resource_name, arn):
 	"""Get data from boto3 (glue) about the given ARN"""
-	
+
 	scan_data_list = []
 	resource_dict = {}
 	resource_internal_state = ""
 
-	resource_type, resource_name = get_resource_structure(arn, arn_components_list, arn_structure_dict)
-	session = boto3.Session(region_name=arn_components_list[arn_structure_dict.get("region")])
-	client = session.client('glue')
-
 	if resource_type == "job":
-		
+
 		# Scan 1: get_job_runs (only return most recent job run)
 		response_get_job_runs = client.get_job_runs(JobName=resource_name, MaxResults=1)
-		
-		if not response_get_job_runs:			
-			return []			
-		else:			
+
+		if not response_get_job_runs:
+			return []
+		else:
 			get_job_runs_dict = response_get_job_runs['JobRuns'][0]
-			resource_dict = { 'get_job_runs': get_job_runs_dict }
+			resource_dict = {'get_job_runs': get_job_runs_dict}
 			resource_internal_state = response_get_job_runs['JobRuns'][0]['JobRunState']
 			scan_data_list.append(resource_dict)
-				
+
 	elif resource_type == "workflow":
-		
+
 		# Scan 1: get_workflow_runs (only return most recent workflow run)
 		response_get_workflow_runs = client.get_workflow_runs(Name=resource_name, MaxResults=1, IncludeGraph=False)
-		
+
 		if not response_get_workflow_runs:
-			return []				
-		else:			
+			return []
+		else:
 			get_workflow_runs_dict = response_get_workflow_runs['Runs'][0]
-			resource_dict = { 'get_workflow_runs': get_workflow_runs_dict }
+			resource_dict = {'get_workflow_runs': get_workflow_runs_dict}
 			resource_internal_state = response_get_workflow_runs['Runs'][0]['Status']
 			scan_data_list.append(resource_dict)
 
 	else:
 		return []
-	
+
 	# list[list[dict], string]
 	return [scan_data_list, resource_internal_state]
 
 
-def scan_lambda(arn, arn_components_list, arn_structure_dict):
+@scanner_aws('lambda')
+def scan_lambda(client, resource_type, resource_name, arn):
 	"""Get data from boto3 (lambda) about the given ARN"""
 
 	scan_data_list = []
 	resource_dict = {}
 	resource_internal_state = ""
-
-	resource_type, resource_name = get_resource_structure(arn, arn_components_list, arn_structure_dict)
-	session = boto3.Session(region_name=arn_components_list[arn_structure_dict.get("region")])
-	client = session.client('lambda')
 
 	if resource_type == "function":
 
@@ -150,47 +170,13 @@ def scan_lambda(arn, arn_components_list, arn_structure_dict):
 	return [scan_data_list, resource_internal_state]
 
 
-def scan_redshift(arn, arn_components_list, arn_structure_dict):
-	"""Get data from boto3 (redshift) about the given ARN"""
-
-	scan_data_list = []
-	resource_dict = {}
-	resource_internal_state = ""
-
-	resource_type, resource_name = get_resource_structure(arn, arn_components_list, arn_structure_dict)
-	session = boto3.Session(region_name=arn_components_list[arn_structure_dict.get("region")])
-	client = session.client('redshift')
-
-	if resource_type == "cluster":
-
-		# Scan 1: describe_clusters
-		response_describe_clusters = client.describe_clusters(ClusterIdentifier=resource_name, MaxRecords=100)
-
-		if not response_describe_clusters:
-			return []
-		else:
-			clusters_dict = response_describe_clusters['Clusters'][0]
-			resource_dict = {'describe_clusters': clusters_dict}
-			resource_internal_state = response_describe_clusters['Clusters'][0]['ClusterAvailabilityStatus']
-			scan_data_list.append(resource_dict)
-
-	else:
-		return []
-
-	# list[list[dict], string]
-	return [scan_data_list, resource_internal_state]
-
-
-def scan_rds(arn, arn_components_list, arn_structure_dict):
+@scanner_aws('rds')
+def scan_rds(client, resource_type, resource_name, arn):
 	"""Get data from boto3 (rds) about the given ARN"""
 
 	scan_data_list = []
 	resource_dict = {}
 	resource_internal_state = ""
-
-	resource_type, resource_name = get_resource_structure(arn, arn_components_list, arn_structure_dict)
-	session = boto3.Session(region_name=arn_components_list[arn_structure_dict.get("region")])
-	client = session.client('rds')
 
 	if resource_type == "cluster":
 
@@ -216,6 +202,34 @@ def scan_rds(arn, arn_components_list, arn_structure_dict):
 			db_instances_dict = response_describe_db_instances['DBInstances'][0]
 			resource_dict = {'describe_db_instances': db_instances_dict}
 			resource_internal_state = response_describe_db_instances['DBInstances'][0]['DBInstanceStatus']
+			scan_data_list.append(resource_dict)
+
+	else:
+		return []
+
+	# list[list[dict], string]
+	return [scan_data_list, resource_internal_state]
+
+
+@scanner_aws('redshift')
+def scan_redshift(client, resource_type, resource_name, arn):
+	"""Get data from boto3 (redshift) about the given ARN"""
+
+	scan_data_list = []
+	resource_dict = {}
+	resource_internal_state = ""
+
+	if resource_type == "cluster":
+
+		# Scan 1: describe_clusters
+		response_describe_clusters = client.describe_clusters(ClusterIdentifier=resource_name, MaxRecords=100)
+
+		if not response_describe_clusters:
+			return []
+		else:
+			clusters_dict = response_describe_clusters['Clusters'][0]
+			resource_dict = {'describe_clusters': clusters_dict}
+			resource_internal_state = response_describe_clusters['Clusters'][0]['ClusterAvailabilityStatus']
 			scan_data_list.append(resource_dict)
 
 	else:
