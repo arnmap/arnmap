d7b85ff1b59bd0a994e93c46aa288f4ebb12c4c0
arnmap <arnmap@proton.me>
2026-09-11 23:52:22 -0500

    argparse semantics tuning

--- arnmap-exec.py
+++ arnmap-exec.py
@@ -14,6 +14,6 @@ def main(args):
 if __name__ == "__main__":
 
     parser = ArgumentParser(description='Scan AWS resources by ARN.')
-    parser.add_argument('--arn', nargs='*', help='List of ARN: "arn1" "arn2" ...', default=[], required=True)
+    parser.add_argument('--arn', nargs='+', help='List of ARN: "arn1" "arn2" ...', required=True)
     args = parser.parse_args()
     main(args)
